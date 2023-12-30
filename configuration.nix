195b3145df6e76581bbbbb3f144aaceb1a702f31
sunder <evenquantity@gmail.com>
2023-12-31 01:27:02 +0300

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  username = "rk";
+in {
+  imports = [ ./hardware-configuration.nix ];
+
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git # used by nix flakes
+    curl
+
+    neofetch
+    lm_sensors # `sensors`
+    btop # monitor system resources
+
+    # Peripherals
+    mtdutils
+    i2c-tools
+    minicom
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = lib.mkDefault true;
+    settings = {
+      X11Forwarding = lib.mkDefault true;
+      PasswordAuthentication = lib.mkDefault true;
+    };
+    openFirewall = lib.mkDefault true;
+  };
+
+  # =========================================================================
+  #      Users & Groups NixOS Configuration
+  # =========================================================================
+
+  # TODO Define a user account. Don't forget to update this!
+  users.users."${username}" = {
+    isNormalUser = true;
+    home = "/home/${username}";
+    extraGroups = ["users" "networkmanager" "wheel" "video" "docker"];
+  };
+
+  users.groups = {
+    "${username}" = {};
+    docker = {};
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
---
+++ hardware-configuration.nix
@@ -0,0 +1,141 @@
+{
+  lib,
+  pkgs,
+  fetchFromGitLab,
+  ...
+}: let
+  mesa-panfork = fetchFromGitLab {
+    owner = "panfork";
+    repo = "mesa";
+    rev =  "120202c675749c5ef81ae4c8cdc30019b4de08f4";
+    hash = "sha256-4eZHMiYS+sRDHNBtLZTA8ELZnLns7yT3USU5YQswxQ0=";
+  };
+in {
+  boot = {
+    kernelPackages = pkgs.linuxPackagesFor (pkgs.callPackage ./pkgs/kernel/legacy.nix {});
+
+    # kernelParams copy from Armbian's /boot/armbianEnv.txt & /boot/boot.cmd
+    kernelParams = [
+      "root=UUID=${rootPartitionUUID}"
+      "rootwait"
+      "rootfstype=ext4"
+
+      "earlycon" # enable early console, so we can see the boot messages via serial port / HDMI
+      "consoleblank=0" # disable console blanking(screen saver)
+      "console=ttyS2,1500000" # serial port
+      "console=tty1" # HDMI
+
+      # docker optimizations
+      "cgroup_enable=cpuset"
+      "cgroup_memory=1"
+      "cgroup_enable=memory"
+      "swapaccount=1"
+    ];
+    # Some filesystems (e.g. zfs) have some trouble with cross (or with BSP kernels?) here.
+    supportedFilesystems = lib.mkForce [
+      "vfat"
+      "fat32"
+      "exfat"
+      "ext4"
+      "btrfs"
+    ];
+
+    loader = {
+      grub.enable = lib.mkForce false;
+      generic-extlinux-compatible.enable = lib.mkForce true;
+    };
+
+    initrd.includeDefaultModules = lib.mkForce false;
+    initrd.availableKernelModules = lib.mkForce ["dm_mod" "dm_crypt" "encrypted_keys"];
+  };
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+
+  hardware = {
+    # driver & firmware for Mali-G610 GPU
+    # it works on all rk2588/rk3588s based SBCs.
+    opengl = {
+      enable = lib.mkDefault true;
+      package =
+        lib.mkForce
+        (
+          (pkgs.mesa.override {
+            galliumDrivers = ["panfrost" "swrast"];
+            vulkanDrivers = ["swrast"];
+          })
+          .overrideAttrs (_: {
+            pname = "mesa-panfork";
+            version = "23.0.0-panfork";
+            src = mesa-panfork;
+          })
+        )
+        .drivers;
+    };
+
+    enableRedistributableFirmware = lib.mkForce true;
+    firmware = [
+      # firmware for Mali-G610 GPU
+      (pkgs.callPackage ./pkgs/mali-firmware {})
+    ];
+
+    # add some missing deviceTree in armbian/linux-rockchip:
+    # orange pi 5's deviceTree in armbian/linux-rockchip:
+    # https://github.com/armbian/linux-rockchip/blob/rk-5.10-rkr4/arch/arm64/boot/dts/rockchip/rk3588s-orangepi-5.dts
+    deviceTree = {
+      name = "rockchip/rk3588s-orangepi-5.dtb";
+      overlays = [
+        {
+          # enable pcie2x1l2 (NVMe), disable sata0
+          name = "orangepi5-sata-overlay";
+          dtsText = ''
+            // Orange Pi 5 Pcie M.2 to sata
+            /dts-v1/;
+            /plugin/;
+
+            / {
+              compatible = "rockchip,rk3588s-orangepi-5";
+
+              fragment@0 {
+                target = <&sata0>;
+
+                __overlay__ {
+                  status = "disabled";
+                };
+              };
+
+              fragment@1 {
+                target = <&pcie2x1l2>;
+
+                __overlay__ {
+                  status = "okay";
+                };
+              };
+            };
+          '';
+        }
+
+        # enable i2c1
+        {
+          name = "orangepi5-i2c-overlay";
+          dtsText = ''
+            /dts-v1/;
+            /plugin/;
+
+            / {
+              compatible = "rockchip,rk3588s-orangepi-5";
+
+              fragment@0 {
+                target = <&i2c1>;
+
+                __overlay__ {
+                  status = "okay";
+                  pinctrl-names = "default";
+                  pinctrl-0 = <&i2c1m2_xfer>;
+                };
+              };
+            };
+          '';
+        }
+      ];
+  };
+}
